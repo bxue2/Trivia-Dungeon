74413db415d70b18971510dd74cbe95e5dd2c439
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-02 00:15:52 -0700

    Adding redux store

---
+++ react-app/src/context/Modal.js
@@ -0,0 +1,36 @@
+import React, {useContext} from 'react'
+
+const ModalContext = React.createContext();
+
+export const ModalProvider = ({children}) => {
+    const modalRef = useRef();
+    const [value, setValue] = useState();
+
+    useEffect(() => {
+      setValue(modalRef.current);
+    }, [])
+
+    return (
+      <>
+        <ModalContext.Provider value={value}>
+          {children}
+        </ModalContext.Provider>
+        <div ref={modalRef} />
+      </>
+    );
+  }
+
+  export function Modal({ onClose, children }) {
+    const modalNode = useContext(ModalContext);
+    if (!modalNode) return null;
+
+    return ReactDOM.createPortal(
+      <div id="modal">
+        <div id="modal-background" onClick={onClose} />
+        <div id="modal-content">
+          {children}
+        </div>
+      </div>,
+      modalNode
+    );
+  }
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Provider} from 'react-redux';
 import App from './App';
+
+import configureStore from './store';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
