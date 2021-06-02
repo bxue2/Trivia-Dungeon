fa75a5e5163b535ca02120c610260d8d2c320863
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-01 23:23:59 -0700

    Setting up navbar

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import { NavLink, useHistory } from 'react-router-dom';
+import LogoutButton from '../auth/LogoutButton';
+
+import './NavBar.css'
 
 const NavBar = ({ setAuthenticated }) => {
+  const history = useHistory();
+  const goHome = () => {
+    history.push('/')
+  }
+
   return (
     <nav>
+      <div className='nav-logo' onClick={goHome}/>
       <ul>
         <li>
           <NavLink to="/" exact={true} activeClassName="active">
