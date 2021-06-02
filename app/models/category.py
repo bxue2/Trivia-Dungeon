7b278aba8d0b8501f6a98d049340baa9555724ce
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-02 09:43:03 -0700

    First migration, minor fixes

--- app/models/category.py
+++ app/models/category.py
@@ -1,7 +1,7 @@
 from .db import db
 
 class Category(db.Model):
-    __tablename__ = 'category'
+    __tablename__ = 'categories'
 
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(80), nullable = False, unique = True)
--- app/models/question.py
+++ app/models/question.py
@@ -7,7 +7,7 @@ class Question(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     question = db.Column(db.String(800), nullable = False, unique = True)
     answer = db.Column(db.String(255), nullable = False)
-    incorrect_answers = db.Column(db.Array(db.String(255)), nullable = False)
+    incorrect_answers = db.Column(db.ARRAY(db.String(255)), nullable = False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
     category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable = False)
     createdAt = db.Column(db.DateTime, default=datetime.utcnow)
