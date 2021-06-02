39d0ffb166267b8129c83f6d96726a501c7be36e
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-02 16:47:29 -0700

    Filling out post question route

--- app/api/question_routes.py
+++ app/api/question_routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from app.models import Question, db
 from app.forms import QuestionForm
+from flask_login import current_user
 
 question_routes = Blueprint('questions', __name__)
 
@@ -10,3 +11,22 @@ def create_question():
     Creates a new question in db.
     """
     form = QuestionForm
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        incorrect_answers_list = [form.data['incorrect_answer_1']]
+        if(form.data['incorrect_answer_2']):
+            incorrect_answers_list.append(form.data['incorrect_answer_2'])
+        if(form.data['incorrect_answer_3']):
+            incorrect_answers_list.append(form.data['incorrect_answer_3'])
+        question = Question(
+            question = form.data['question'],
+            answer = form.data['answer'],
+            incorrect_answers = incorrect_answers_list,
+            difficulty = form.data['difficulty'],
+            user_id = current_user.to_dict()['id'],
+            category_id = form.data['category']
+        )
+        db.session.add(question)
+        db.session.commit()
+        return question.to_dict()
+    return {'errors': form.errors}, 401
