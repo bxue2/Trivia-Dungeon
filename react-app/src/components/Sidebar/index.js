8870931b06bc76265503b37911cf602991bc24a5
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-03 08:29:00 -0700

    Added basic sidebar links + prompt if not logged in

--- react-app/src/components/Sidebar/index.js
+++ react-app/src/components/Sidebar/index.js
@@ -1,12 +1,39 @@
 import React, {useState} from 'react'
-
+import {useSelector} from 'react-redux';
+import {useHistory} from 'react-router-dom';
 import './Sidebar.css'
 
 const Sidebar = () => {
+    const user = useSelector(state => state.session.user);
+    const history = useHistory();
+
+    let sidebarLinks = (
+        <div className='sign-up-prompt'>
+            <div className='prompt-body'>
+                Please sign up/log in to unlock these functions
+            </div>
+        </div>
+    )
+
+    if(user){
+        sidebarLinks = (
+            <>
+                <div className='sidebar-link' onClick={() => history.push('/questions')}>
+                    Questions
+                </div>
+                <div className='sidebar-link' onClick={() => history.push('/sets')}>
+                    Sets
+                </div>
+            </>
+        )
+    }
+
     // const [showBar, setShowBar] = useState(true)
     return (
         <div className='sidebar-container'>
             {/* <div className='sidebar-controller'></div> */}
+            {sidebarLinks}
+
         </div>
     )
 }
