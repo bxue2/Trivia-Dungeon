fb2fdd4969b16f67408ec25f294a63385b9a1fbf
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-03 08:59:20 -0700

    Added demo login

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import LogoutButton from './LogoutButton';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import {login} from '../../store/session'
 
 import './NavBar.css'
 import LoginModal from './LoginModal';
@@ -9,13 +10,21 @@ import SignUpModal from './SignUpModal';
 
 const NavBar = () => {
   const user = useSelector(state => state.session.user);
+  const dispatch = useDispatch();
   const history = useHistory();
   const goHome = () => {
     history.push('/')
   }
 
+  const demoLogin = () => {
+    dispatch(login('demo@aa.io', 'password'))
+  }
+
   let authButtons = (
     <>
+      <button className='demo-button' onClick={demoLogin}>
+        Demo
+      </button>
       <LoginModal />
       <SignUpModal />
     </>
