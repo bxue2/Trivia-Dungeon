f29cecf695ccc9ca5df96c53942581b0f77d2533
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-03 15:53:02 -0700

    Add skeleton/route to QuestionPage

---
+++ react-app/src/components/QuestionPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useParams} from 'react-router-dom';
+
+const QuestionPage = () => {
+    const {id} = useParams();
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default QuestionPage;
