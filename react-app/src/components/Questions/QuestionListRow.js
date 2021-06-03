66a04d57209e11a7313a3c4593a31e7fd9b77790
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-03 11:03:03 -0700

    Add basic styling to question rows

--- react-app/src/components/Questions/QuestionListRow.js
+++ react-app/src/components/Questions/QuestionListRow.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const QuestionListRow = ({question}) => {
+    console.log(question)
     return (
         <div className='question-list-row'>
-            <p>{question.id}</p>
-            <button className='edit-button'>Edit</button>
-            <button className='delete-button'>Delete</button>
+            <p className='question-display'>Question: {question.question}</p>
+            <div className='question-row_button-container'>
+                <button className='edit-button'>Edit</button>
+                <button className='delete-button'>Delete</button>
+            </div>
         </div>
     )
 }
