9cea96cfe53d635302a9a535e838c9fc1b99cff4
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-03 11:25:12 -0700

    More setting up form

--- react-app/src/components/Questions/AddQuestion.js
+++ react-app/src/components/Questions/AddQuestion.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const AddQuestion = () => {
+const AddQuestion = ({origQuestion}) => {
+    const [errors, setErrors] = useState([]);
+    const [question, setQuestion] = useState("");
+
+    const checkErrors = () => {
+        let newErrors = [];
+        if(question.length == 0){
+            newErrors.push('Question field is required.')
+        }
+        setErrors(newErrors);
+    }
+
+    const submitQuestion = (e) => {
+        e.preventDefault()
+        checkErrors();
+        if(errors.length === 0){
+            const response = await fetch('/api/questions', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    'question': question
+                })
+            })
+            const res_question = await response.json();
+        }
+    }
     return (
         <div className='add-question-form'>
-
+            <form onSubmit={(e) => submitQuestion(e)}>
+            <div>
+                {errors.map((error, idx) => (
+                <div className="error-div" key={idx}>
+                    {error}
+                </div>
+                ))}
+            </div>
+            <div className='add_question_field-row'>
+                <input
+                    type='text'
+                    name='question'
+                    placeholder='Question'
+                    value={question}
+                    onChange={(e) => setQuestion(e.target.value)}
+                />
+            </div>
+            <button type='submit' className='add-question-submit'>
+                {origQuestion ? 'Edit' : 'Submit'}
+            </button>
+            </form>
         </div>
     )
 }
