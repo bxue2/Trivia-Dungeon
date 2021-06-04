3f828de032a68cb6a389585afe94a8321b0061bc
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-04 15:16:23 -0700

    getRandomQuestions randomizes order now

--- react-app/src/store/questions.js
+++ react-app/src/store/questions.js
@@ -12,6 +12,16 @@ const nextQuestion = () => ({
 
 const initialState = { questions: []};
 
+//In-place
+const shuffleArray = (arr) => {
+    let newArr = arr.slice();
+    for(let i = newArr.length - 1; i > 0; i--){
+        let randomIndex = Math.floor(Math.random() * (i + 1));
+        [newArr[i], newArr[randomIndex]] = [newArr[randomIndex], newArr[i]];
+    }
+    return newArr;
+}
+
 //Gets all questions associated with setId
 export const getQuestionsFromSet = (setId) => async (dispatch) => {
 
@@ -25,11 +35,12 @@ export const getQuestionsFromCategories = (categories, num) => async (dispatch) 
 //Gets 30 random questions from entire dataset
 export const getRandomQuestions = () => async (dispatch) => {
     const response = await fetch("/api/questions/random")
-    const data = await response.json();
+    let data = await response.json();
+    let newData = shuffleArray(data.questions);
     if(!data.errors){
-        dispatch(getQuestions(data))
+        dispatch(getQuestions(newData))
     }
-    return data;
+    return newData;
 }
 
 export const goToNextQuestion = () => (dispatch) => {
@@ -39,10 +50,10 @@ export const goToNextQuestion = () => (dispatch) => {
 export default function reducer(state=initialState, action) {
     switch (action.type){
         case GET_QUESTIONS:
-            return action.payload
+            return {questions: action.payload}
         case NEXT_QUESTION:
             let newQuestions = state.questions.slice(1);
-            return newQuestions
+            return {questions: newQuestions}
         default:
             return state;
     }
