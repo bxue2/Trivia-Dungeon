0119b7af1460663498e7738b44b1dbfff22f561a
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-04 09:42:41 -0700

    Answer buttons

--- react-app/src/components/QuestionPage/index.js
+++ react-app/src/components/QuestionPage/index.js
@@ -4,6 +4,8 @@ import {useParams} from 'react-router-dom';
 import TriviaRender from '../TriviaRender';
 import CommentSection from './CommentSection';
 
+import './QuestionPage.css'
+
 const QuestionPage = () => {
     const {id} = useParams();
 
