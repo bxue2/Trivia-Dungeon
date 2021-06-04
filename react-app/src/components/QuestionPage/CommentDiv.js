94281ddc90576b6f1e72468e3980c6df9bb5e9b8
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-04 11:10:47 -0700

    Extra comment styling

--- react-app/src/components/QuestionPage/CommentDiv.js
+++ react-app/src/components/QuestionPage/CommentDiv.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Rating from '@material-ui/lab/Rating';
 
 const CommentDiv = ({comment}) => {
     return (
-        <div>
+        <div className='comment-div'>
+            <Rating
+                name="rating-buttons"
+                value={comment.rating}
+                readOnly
+            />
+            <div>
+                {comment.comment}
+            </div>
 
         </div>
     )
