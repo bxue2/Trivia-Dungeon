93ec9ceb7f609fc34ede44341fe6fdb15cded123
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-04 08:41:17 -0700

    Installed Material, leaving some experimental code for logout button

--- react-app/src/components/NavBar/LogoutButton.js
+++ react-app/src/components/NavBar/LogoutButton.js
@@ -2,6 +2,24 @@ import React from "react";
 import {useDispatch} from 'react-redux';
 import { logout } from "../../store/session";
 
+// import Button from '@material-ui/core/Button';
+// import { makeStyles } from '@material-ui/core/styles';
+// import { red } from '@material-ui/core/colors';
+
+// const useStyles = makeStyles({
+//   root:{
+//     backgroundColor: red[800],
+//     color: "white",
+//     fontSize: "16px",
+//     width: "100px",
+//     padding: "10px",
+//     borderRadius: "10px",
+//     '&:hover': {
+//       backgroundColor: red[600],
+//     },
+//   }
+// })
+
 const LogoutButton = () => {
   const dispatch = useDispatch();
 
@@ -10,6 +28,7 @@ const LogoutButton = () => {
   };
 
   return <button className='logout-button' onClick={onLogout}>Logout</button>;
+  // return <Button classes={{root: useStyles().root}} variant='contained' onClick={onLogout}>Logout</Button>;
 };
 
 export default LogoutButton;
