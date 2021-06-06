fa291c6b4ab033a7c3a03ae41abf3f9252edebd6
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-05 22:54:18 -0700

    Fixed seeding, works now

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -3,9 +3,10 @@ from app.seeds.sets import seed_sets
 from app.seeds.questions import seed_questions
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
-from .questions import seed_questions, undo_questions
+from .questions import seed_questions, seed_categories, undo_questions, undo_categories
 from .qcomments import seed_qcomments, undo_qcomments
 from .sets import seed_sets, undo_sets
+from .opentdb import seed_questions_from_opentdb
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -15,7 +16,9 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_categories()
     seed_questions()
+    seed_questions_from_opentdb()
     seed_qcomments()
     seed_sets()
     # Add other seed functions here
@@ -25,6 +28,7 @@ def seed():
 def undo():
     undo_users()
     undo_questions()
+    undo_categories()
     undo_qcomments()
     undo_sets()
     # Add other undo functions here
