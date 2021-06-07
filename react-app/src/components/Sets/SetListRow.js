ed26066dd1a50c5d7395b64e55129ab7cae3c592
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-07 00:25:38 -0700

    Adding set list row component

--- react-app/src/components/Sets/SetListRow.js
+++ react-app/src/components/Sets/SetListRow.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import {useHistory} from 'react-router-dom';
 
-const SetListRow = () => {
+const SetListRow = ({set, getSets, setEditSet}) => {
+    const history = useHistory();
+
+    const deleteSet = async () => {
+        await fetch(`/api/sets/${set.id}`, {
+            method: 'DELETE'
+        })
+        // const deleted = await response.json();
+        getSets();
+    }
+
+    const openEditForm = () => {
+        setEditSet(set);
+    }
+
+    const viewSet = () => {
+        history.push(`/sets/${set.id}`);
+    }
+
     return (
-        <div>
-
+        <div className='set-list-row'>
+            <p className='set-display'>Set: {set.name}</p>
+            <div className='set-row_button-container'>
+                <button className='view-button' onClick={viewSet}>View</button>
+                <button className='edit-button' onClick={openEditForm}>Edit</button>
+                <button className='delete-button' onClick={deleteSet} >Delete</button>
+            </div>
         </div>
     )
 }
