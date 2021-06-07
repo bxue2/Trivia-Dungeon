99633e8ea290d35e37ea9c1a1fcf014073d31e59
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-06 23:49:38 -0700

    Filling in create set form

--- react-app/src/components/Sets/AddSet.js
+++ react-app/src/components/Sets/AddSet.js
@@ -1,8 +1,14 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const AddSet = ({setShowForm, editSet, setEditSet, getSets}) => {
     const [name, setName] = useState("");
     const [errors, setErrors] = useState([]);
+
+    const clearFields = () => {
+        setName("");
+        setShowForm(false);
+        setEditSet(null);
+    }
 
     const checkErrors = () => {
         let newErrors = [];
@@ -13,12 +19,37 @@ const AddSet = ({setShowForm, editSet, setEditSet, getSets}) => {
         return newErrors;
     }
 
-    const createSet = (e) => {
+    //Prefill if editing
+    useEffect(() => {
+        if(editSet){
+            setName(editSet.name);
+        }
+    }, [editSet])
+
+    const createSet = async (e) => {
         e.preventDefault();
         let newErrors = checkErrors();
+        let fetchMethod = 'POST';
+        let putId = '';
+        if(editSet){
+            fetchMethod = 'PUT';
+            putId = editSet.id;
+        }
+        if(newErrors.length === 0){
+            await fetch(`/api/sets/${putId}`, {
+                method: fetchMethod,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    'name': name
+                })
+            })
+            // const res_question = await response.json();
+            getSets();
+            clearFields();
+        }
     }
-
-
 
     return (
         <div className='add-set-container'>
@@ -31,28 +62,26 @@ const AddSet = ({setShowForm, editSet, setEditSet, getSets}) => {
                     </div>
                     ))}
                 </div>
-                <label htmlFor='setName'>Set Name:</label>
-                <input
-                    type='text'
-                    name='setName'
-                    placeholder='Name'
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
+                <div className='add_set_field-row'>
+                    <label htmlFor='setName'>Set Name:</label>
+                    <input
+                        type='text'
+                        name='setName'
+                        placeholder='Name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
                 <div>
                     <button type='submit' className='add-set-submit'>
                         {editSet ? 'Edit' : 'Create'}
                     </button>
-                    <button className='add-set-cancel' onClick={() => {
-                        setName("")
-                        setShowForm(false)}}>
+                    <button className='add-set-cancel' onClick={() => clearFields()}>
                         Cancel
                     </button>
                 </div>
             </form>
-
         </div>
-
     )
 }
 
