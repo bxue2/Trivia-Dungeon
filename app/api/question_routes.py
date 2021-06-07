7c8a3b73fa1e70ca22001a53de92bea277eeab12
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-07 14:18:55 -0700

    Added category query

--- app/api/question_routes.py
+++ app/api/question_routes.py
@@ -48,9 +48,12 @@ def get_random_question():
     Gets up to 30 random questions.
     """
     args = request.args
-    print("Test{0}".format(args.get('categories')))
     num = int(args.get('num')) if args.get('num') else 30
-    questions = Question.query.order_by(func.random()).limit(num).all()
+    queries = []
+    if args.get('category1'):
+        queries.append(Question.category_id == args.get('category1'))
+
+    questions = Question.query.filter(*queries).order_by(func.random()).limit(num).all()
     return {"questions": [question.to_dict() for question in questions]}
 
 @question_routes.route('/user/<int:userid>')
