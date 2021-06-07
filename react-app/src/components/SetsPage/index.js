df58e9801b3e03f5491b286ce8e25e67cbdad473
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-07 09:39:37 -0700

    Filling in sets page

--- react-app/src/components/SetsPage/index.js
+++ react-app/src/components/SetsPage/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+import {useParams, useHistory } from 'react-router-dom';
+
+import TriviaRender from '../TriviaRender';
 
 const SetsPage = () => {
+    const {id} = useParams();
+    const history = useHistory();
+
+    const [set, setSet] = useState({})
+
+    const getSets = useCallback(async () => {
+        const response = await fetch(`/api/sets/${id}`);
+        const setInfo = await response.json();
+        if(setInfo.errors){
+            history.push('/');
+        }
+        else{
+            setSet(setInfo);
+        }
+    }, [id, history]);
+
+    useEffect(() => {
+        getSets();
+    }, [getSets])
+
     return (
-    <div>
+    <div className='sets-page'>
+        {set.name}
         Sets Page
     </div>)
 }
