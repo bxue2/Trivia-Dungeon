eddd5f73982efc30b0bf27c4e986f079b233b20c
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-08 15:08:12 -0700

    Working search api route

--- react-app/src/components/SearchResults/index.js
+++ react-app/src/components/SearchResults/index.js
@@ -1,19 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import ListComponent from '../ListComponent';
 import { useLocation } from "react-router-dom";
 import queryString from 'query-string';
 
 const SearchResults = (props) => {
-    const [queryStr, setQueryStr] = useState();
+    const [questions, setQuestions] = useState();
     const query = useLocation()
+
+    const getSearch = useCallback(async (queryStr) => {
+        const response = await fetch(`/api/questions/search?query=${queryStr}`)
+        const data = await response.json()
+        // if(data.errors){
+        // } else{
+            console.log(data.questions)
+            setQuestions(data.questions);
+        // }
+    }, [setQuestions])
 
     useEffect(() => {
         const values = queryString.parse(query.search)
         if(values.query){
-            setQueryStr(values.query);
+            getSearch(values.query);
         }
-        
-    }, [query])
+
+    }, [query, getSearch])
     return (
         <div>
             <ListComponent>
