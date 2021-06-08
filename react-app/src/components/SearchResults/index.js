4336a651f8223489371825ca0975f5875b4b6fe1
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-08 14:34:37 -0700

    Adding search question route

--- react-app/src/components/SearchResults/index.js
+++ react-app/src/components/SearchResults/index.js
@@ -1,16 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ListComponent from '../ListComponent';
 import { useLocation } from "react-router-dom";
 import queryString from 'query-string';
 
 const SearchResults = (props) => {
+    const [queryStr, setQueryStr] = useState();
     const query = useLocation()
 
     useEffect(() => {
-        const values = queryString.parse(query)
-        console.log(values)
+        const values = queryString.parse(query.search)
         if(values.query){
+            setQueryStr(values.query);
         }
+        
     }, [query])
     return (
         <div>
