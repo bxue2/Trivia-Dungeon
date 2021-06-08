35388bdd14a718765ca402a1ba5ac722f86dc82b
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-08 14:28:35 -0700

    Figuring out query strings in react

--- react-app/src/components/SearchResults/index.js
+++ react-app/src/components/SearchResults/index.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ListComponent from '../ListComponent';
+import { useLocation } from "react-router-dom";
+import queryString from 'query-string';
 
-const SearchResults = () => {
+const SearchResults = (props) => {
+    const query = useLocation()
+
+    useEffect(() => {
+        const values = queryString.parse(query)
+        console.log(values)
+        if(values.query){
+        }
+    }, [query])
     return (
         <div>
             <ListComponent>
