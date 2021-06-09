88d6107c7c24430543b48ca45755397a2efd5444
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-08 17:16:54 -0700

    Can now login with either username or email

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, session, request
 from app.models import User, db
 from app.forms import LoginForm
 from app.forms import SignUpForm
+from sqlalchemy import or_
 from flask_login import current_user, login_user, logout_user, login_required
 
 auth_routes = Blueprint('auth', __name__)
@@ -40,7 +41,8 @@ def login():
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         # Add the user to the session, we are logged in!
-        user = User.query.filter(User.email == form.data['email']).first()
+        print(form.data['email'])
+        user = User.query.filter(((User.username == form.data['email']) | (User.email == form.data['email'] ))).first()
         login_user(user)
         return user.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
