f565775dc9299cd64b4fa3d2fd601dcd8dcb8a46
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-09 13:09:18 -0700

    Adding more styling, buttons

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -7,7 +7,11 @@ const SplashPage = () => {
         Splash Page
         <div className='splash-info'>
             <div className='trivia-header'>Welcome to the Trivia Dungeon</div>
-            <div>Sign up to submit your own questions and create question sets.</div>
+            <div className='trivia-subheader'>Sign up to submit your own questions and create question sets.</div>
+            <div className='trivia-options'>
+                <button className='splash-button'>Try some random questions</button>
+                <button className='splash-button'>Sign up for an account</button>
+            </div>
         </div>
         <img className='dungeon-door-bg' src={door}/>
     </div>)
