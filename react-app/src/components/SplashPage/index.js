69b315feefcb9ab66f0d270e830381829fff3579
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-09 13:36:30 -0700

    Buttons on splash page work now

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import {useHistory} from 'react-router-dom';
 import door from '../../pictures/dungeon_door_tp.png'
 import './SplashPage.css'
 const SplashPage = () => {
+    const history = useHistory()
     return (
     <div className='splash-container'>
         Splash Page
@@ -9,8 +11,8 @@ const SplashPage = () => {
             <div className='trivia-header'>Welcome to the Trivia Dungeon</div>
             <div className='trivia-subheader'>Sign up to submit your own questions and create question sets.</div>
             <div className='trivia-options'>
-                <button className='splash-button'>Try some random questions</button>
-                <button className='splash-button'>Sign up for an account</button>
+                <button className='splash-button' onClick={() => history.push('/random')}>Try some random questions</button>
+                <button className='splash-button' onClick={() => {document.querySelector('.signup-button').click()}}>Sign up for an account</button>
             </div>
         </div>
         <img className='dungeon-door-bg' src={door}/>
