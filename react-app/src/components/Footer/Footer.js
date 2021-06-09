9bef9ecdd75dc26f133e468fbb129c34bf00d1f5
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-09 15:29:01 -0700

    Make entering website more obvious

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -5,7 +5,9 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className='footer-container'>
-            <a className='attribute-link' href='https://www.freepik.com/vectors/house'>House vector created by vectorpocket - www.freepik.com</a>
+            <div className='profile-pic'></div>
+            <div className='link-section'></div>
+            {/* <a className='attribute-link' href='https://www.freepik.com/vectors/house'>House vector created by vectorpocket - www.freepik.com</a> */}
         </div>
     )
 }
