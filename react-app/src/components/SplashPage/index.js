d0709661d14bff5c439b25c84385093734328045
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-09 11:15:40 -0700

    Setting up splash page

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -2,8 +2,10 @@ import React from 'react'
 import './SplashPage.css'
 const SplashPage = () => {
     return (
-    <div>
+    <div className='splash-container'>
         Splash Page
+        <div className='trivia-header'>Welcome to the Trivia Dungeon</div>
+        <div className='dungeon-door-bg' />
     </div>)
 }
 
