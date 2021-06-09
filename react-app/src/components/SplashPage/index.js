7de18bd36b98adb734a2b5fa60eafb6941e2ac2b
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-09 12:52:19 -0700

    Adding styling

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -5,7 +5,10 @@ const SplashPage = () => {
     return (
     <div className='splash-container'>
         Splash Page
-        <div className='trivia-header'>Welcome to the Trivia Dungeon</div>
+        <div className='splash-info'>
+            <div className='trivia-header'>Welcome to the Trivia Dungeon</div>
+            <div>Sign up to submit your own questions and create question sets.</div>
+        </div>
         <img className='dungeon-door-bg' src={door}/>
     </div>)
 }
