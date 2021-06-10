e3048803279a8598526ff3278f8de4de48c250f2
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-10 15:48:11 -0700

    Filling out setquestionlist component

--- react-app/src/components/SetQuestionList/SetQuestionRow.js
+++ react-app/src/components/SetQuestionList/SetQuestionRow.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import useHistory from 'react-router-dom';
+const SetQuestionRow = ({question}) => {
+    const history = useHistory();
 
-const SetQuestionRow = () => {
+    const viewQuestion = () => {
+        history.push(`/questions/${question.id}`)
+    }
 
+    const removeQuestion = () => {
+
+    }
+    return (
+        <div className='set-question-row'>
+            <p className='set-question-display'>Question: {question.question}</p>
+            <div className='set-question-row_button-container'>
+                <button className='view-button question-row_action-button' onClick={viewQuestion}>View</button>
+                {getQuestions && <button className='delete-button question-row_action-button' onClick={removeQuestion} >Remove</button>}
+
+            </div>
+        </div>
+    )
 }
 
 export default SetQuestionRow;
--- react-app/src/components/SetQuestionList/index.js
+++ react-app/src/components/SetQuestionList/index.js
@@ -6,32 +6,34 @@ const SetQuestionList = ({set}) => {
     const [questions, setQuestions] = useState([])
     const [loaded, setLoaded] = useState(false)
 
-    const getQuestions = useCallback(async (queryStr, queryType) => {
-        const response = await fetch(`/api/${queryType}/search?${queryStr ? `query=${queryStr}` : ''}${(values.category1 && (values.category1 != 0)) ? '&category1=' + values.category1 : ''}`)
-        const searchData = await response.json()
+    const getQuestions = useCallback(async () => {
+        const response = await fetch(`/api/sets/${set.id}`)
+        const data = await response.json()
         // if(data.errors){
         // } else{
-        if(queryType === 'questions'){
-            setQuestions(searchData.questions);
-        } else{
-            console.log(searchData)
-            setSets(searchData.sets);
-        }
-        setLoaded(true);
+        setQuestions(data.questions);
+    }, [setQuestions])
 
-        // }
-    }, [setQuestions, query])
+    useEffect(() => {
+        getQuestions();
+    }, [getQuestions])
+
+    useEffect(() => {
+        if(questions.length !== 0){
+            setLoaded(true)
+        }
+    }, [questions, setLoaded])
 
     return (
-        <ListComponent title='Search Results'>
-            {questions && sets.map((set, idx) => {
+        <ListComponent title='Question List'>
+            {questions && questions.map((set, idx) => {
                 return (
-                    <SetListRow set={set} key={idx}/>
+                    <SetQuestionRow question={question} key={idx}/>
                 )})
             }
             {(questions.length === 0 && sets.length === 0 && loaded) &&
             <div className='no-results-screen'>
-                No results found.
+                No questions found.
             </div>}
         </ListComponent>
     )
