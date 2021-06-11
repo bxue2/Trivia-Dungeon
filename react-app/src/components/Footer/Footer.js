5fed084019149bac15fe22df47ffeeb74c661745
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-11 09:35:09 -0700

    Added github icon, links

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import profile from '../../pictures/brian-headshot.png'
 import linkedin from '../../pictures/LI-In-Bug.png'
+import github from '../../pictures/GitHub-Mark-Light-120px-plus.png'
 import './Footer.css'
 
 const Footer = () => {
@@ -10,10 +11,21 @@ const Footer = () => {
                 <img className='profile-pic' src={profile}></img>
             </div>
             <div className='link-section'>
-                <img className='link-image' src={linkedin}></img>
+                <a href='https://www.linkedin.com/in/brian-xue/'>
+                    <img className='link-image' src={linkedin} ></img>
+                </a>
+                <a href='https://github.com/bxue2'>
+                    <img className='link-image' src={github}></img>
+                </a>
             </div>
-            <div className='profile-desc'></div>
-            <div className='credit-section'>Trivia questions seeded from <a href='https://opentdb.com/'>OpenTdb</a></div>
+            <div className='profile-desc'>Trivia Dungeon was created by Brian Xue.</div>
+            <div className='filler-div'></div>
+            <div className='credit-section'>
+                <div>Credits:</div>
+                <div>
+                    Trivia questions seeded from <a href='https://opentdb.com/'>OpenTdb</a>
+                </div>
+            </div>
             {/* <a className='attribute-link' href='https://www.freepik.com/vectors/house'>House vector created by vectorpocket - www.freepik.com</a> */}
         </div>
     )
