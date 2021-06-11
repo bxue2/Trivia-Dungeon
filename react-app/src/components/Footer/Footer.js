e60941a23846956bd07d9caccb9d37b0282aa3c2
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-11 09:25:01 -0700

    Navbar sizing fixes

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import door from '../../pictures/brian-headshot.png'
+import profile from '../../pictures/brian-headshot.png'
+import linkedin from '../../pictures/LI-In-Bug.png'
 import './Footer.css'
 
 const Footer = () => {
     return (
         <div className='footer-container'>
             <div className='profile-pic-container'>
-                <img className='profile-pic' src={door}></img>
+                <img className='profile-pic' src={profile}></img>
             </div>
-            <div className='link-section'></div>
+            <div className='link-section'>
+                <img className='link-image' src={linkedin}></img>
+            </div>
+            <div className='profile-desc'></div>
+            <div className='credit-section'>Trivia questions seeded from <a href='https://opentdb.com/'>OpenTdb</a></div>
             {/* <a className='attribute-link' href='https://www.freepik.com/vectors/house'>House vector created by vectorpocket - www.freepik.com</a> */}
         </div>
     )
