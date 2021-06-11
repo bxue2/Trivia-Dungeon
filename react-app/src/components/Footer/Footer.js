7280d13df1a425db738ad652ea06147926ebd058
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-06-11 09:04:42 -0700

    Added profile image, trivia render sizing on heroku

--- react-app/src/components/Footer/Footer.js
+++ react-app/src/components/Footer/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import door from '../../pictures/brian-headshot.png'
 import './Footer.css'
 
 const Footer = () => {
     return (
         <div className='footer-container'>
-            <div className='profile-pic'></div>
+            <div className='profile-pic-container'>
+                <img className='profile-pic' src={door}></img>
+            </div>
             <div className='link-section'></div>
             {/* <a className='attribute-link' href='https://www.freepik.com/vectors/house'>House vector created by vectorpocket - www.freepik.com</a> */}
         </div>
